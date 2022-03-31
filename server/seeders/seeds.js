e372c7816779b8a58f0c72b4f5fbf3e0f76d7423
Subeom Jeong <subeomjeong@Subeoms-MBP.attlocal.net>
2022-03-30 21:52:57 -0700

    updated seed js

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -11,43 +11,37 @@ db.once('open', async () => {
   const userData = [];
 
   for (let i = 0; i < 50; i += 1) {
-    const user_id = faker.datatype.number({ max: 1000 });
+    let user_id = faker.datatype.number({ max: 1000 });
     const email = faker.internet.email();
     const password = faker.internet.password();
 
     userData.push({ user_id, email, password });
   }
 
-  const createdUsers = await User.collection.insertMany(userData);
-
-
   // create sections
-  let createdSections = [];
+  const sectionData = [];
+
   for (let i = 0; i < 100; i += 1) {
-    const sectionText = faker.datatype.number({ min: 10, max: 100, precision: 0 });
+    let section_id = faker.datatype.number({ min: 10, max: 100, precision: 0 });
+    const section_name = faker.name.jobType();
+    const section_description = faker.lorem.sentence(5);
 
-    const createdSection = await Section.create({ sectionText });
+    sectionData.push({ section_id, section_name, section_description });
 
-    createdSection.push(createdSection);
   }
 
   // create assessment
+  const assessmentData = [];
+
   for (let i = 0; i < 100; i += 1) {
-    const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const assessment_id = faker.datatype.number({ max: 1000 });
+    const assessment_name = faker.commerce.product();
+    const assessment_start_date = faker.date.recent();
+    const assessment_end_date = faker.date.soon();
 
-    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { username } = createdUsers.ops[randomUserIndex];
-
-    const randomThoughtIndex = Math.floor(Math.random() * createdThoughts.length);
-    const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
-
-    await Thought.updateOne(
-      { _id: thoughtId },
-      { $push: { reactions: { reactionBody, username } } },
-      { runValidators: true }
-    );
+    assessmentData.push({ assessment_id, assessment_name, user_id, section_id, assessment_start_date, assessment_end_date });
   }
+  
+  return { userData, sectionData, assessmentData };
 
-  console.log('all done!');
-  process.exit(0);
 });
